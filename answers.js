10385cedefa2306aadea3b7690966a68d5c35d9a
tony khoury <tony_khoury993@hotmail.com>
2020-03-29 11:51:03 +0200

    initiatives

---
+++ answers.js
@@ -0,0 +1,7 @@
+// document.getElementById("myForm").addEventListener("click", function(event) {
+//   event.preventDefault();
+// });
+
+function answer() {
+  document.querySelector("label").innerHTML = "hey";
+}
