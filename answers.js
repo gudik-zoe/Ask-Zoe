fe25837c8793df368fe0180207223e80a2c8a260
tony khoury <tony_khoury993@hotmail.com>
2020-03-30 12:29:45 +0200

    some updates

--- answers.js
+++ answers.js
@@ -11,8 +11,14 @@ function answer() {
     case "how old are you":
       a = "no idea...ask my maker";
       break;
+    default:
+      a = "don't know about that yet :( will ask my maker to teach me ";
+      break;
   }
-  document.querySelector("label").innerHTML = a;
+  (document.querySelector("label").innerHTML = "processing.."),
+    setTimeout(() => {
+      document.querySelector("label").innerHTML = a;
+    }, 2000);
 }
 
 function style() {
